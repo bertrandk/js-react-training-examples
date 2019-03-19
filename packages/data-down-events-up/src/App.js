5bf3425e886275ec03e86e44bd945a40f3f92f59
Evan Schultz <evan@rangle.io>
2019-03-19 16:07:26 -0400

    basic callback example

--- packages/data-down-events-up/src/App.js
+++ packages/data-down-events-up/src/App.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HelloWorld from "./components/HelloWorld";
 
 class App extends Component {
+  state = {
+    helloWorldCount: 0,
+    helloWorldTwo: 0
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleClickOne = this.handleClickOne.bind(this);
+    this.handleClickTwo = this.handleClickTwo.bind(this);
+  }
+  handleClickOne(value) {
+    this.setState({ helloWorldOne: value });
+  }
+  handleClickTwo(value) {
+    this.setState({ helloWorldTwo: value });
+  }
   render() {
     return (
       <div className="App">
-        Demo App to later show data down events up
+        <ul>
+          <li>First Hello World Clicked: {this.state.helloWorldOne}</li>
+          <li>First Hello World Clicked: {this.state.helloWorldTwo}</li>
+        </ul>
+        <HelloWorld onValueChange={this.handleClickOne} />
+        <br />
+        
+        <HelloWorld onValueChange={this.handleClickTwo} />
       </div>
     );
   }
